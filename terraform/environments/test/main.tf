8b029be4fd18abc7dadad8fc5311802f6bf9398f
Luiyi Hurtado <75503930+Luiyi-Hurtado@users.noreply.github.com>
2024-01-04 17:05:31 +0100

    add all files

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
+  tenant_id       = "${var.tenant_id}"
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
   features {}
 }
 terraform {
@@ -14,41 +14,49 @@ terraform {
   }
 }
 module "resource_group" {
-  source         = "../../modules/resource_group"
-  resource_group = var.resource_group
-  location       = var.location
+  source               = "../../modules/resource_group"
+  resource_group       = "${var.resource_group}"
+  location             = "${var.location}"
+}
+resource "azurerm_resource_group" "Azuredevops" {
+  name     = "Azuredevops"
+  location = "${var.location}"
 }
 module "network" {
   source               = "../../modules/network"
-  address_space        = var.address_space
-  location             = var.location
-  virtual_network_name = var.virtual_network_name
-  application_type     = var.application_type
+  address_space        = "${var.address_space}"
+  location             = "${var.location}"
+  virtual_network_name = "${var.virtual_network_name}"
+  application_type     = "${var.application_type}"
   resource_type        = "NET"
-  resource_group       = module.resource_group.resource_group_name
-  address_prefix_test  = var.address_prefix_test
+  resource_group       = "${module.resource_group.resource_group_name}"
+  address_prefix_test  = "${var.address_prefix_test}"
 }
-
 module "nsg-test" {
-  source              = "../../modules/networksecuritygroup"
-  location            = var.location
-  application_type    = var.application_type
-  resource_type       = "NSG"
-  resource_group      = module.resource_group.resource_group_name
-  subnet_id           = module.network.subnet_id_test
-  address_prefix_test = var.address_prefix_test
-}
-module "appservice" {
-  source           = "../../modules/appservice"
-  location         = var.location
-  application_type = var.application_type
-  resource_type    = "AppService"
-  resource_group   = module.resource_group.resource_group_name
+  source           = "../../modules/networksecuritygroup"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "NSG"
+  resource_group   = "${module.resource_group.resource_group_name}"
+  subnet_id        = "${module.network.subnet_id_test}"
+  address_prefix_test = "${var.address_prefix_test}"
 }
 module "publicip" {
   source           = "../../modules/publicip"
-  location         = var.location
-  application_type = var.application_type
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
   resource_type    = "publicip"
-  resource_group   = module.resource_group.resource_group_name
+  resource_group   = "${module.resource_group.resource_group_name}"
 }
+# module "vm" {
+#   source               = "../../modules/vm"
+#   location             = "${var.location}"
+#   resource_group       = "${module.resource_group.resource_group_name}"
+#   application_type     = "${var.application_type}"
+#   subnet_id            = "${module.network.subnet_id_test}"
+#   vm_admin_username    = "adminuser"
+#   public_ip_address_id = "${module.publicip.public_ip_address_id}"
+#   packer_image         = "/subscriptions/a4b11da3-2642-4ae2-b8e0-ba40545a13d6/resourceGroups/Azuredevops/providers/Microsoft.Compute/images/myPackerImage"
+#   vm_size              =  "Standard_B1s"
+#   resource_type        =  "webapp"
+# }
--- terraform/modules/resource_group/rg.tf
+++ terraform/modules/resource_group/rg.tf
@@ -1,4 +1,12 @@
-resource "azurerm_resource_group" "test" {
+resource "azurerm_resource_group" "Azuredevops" {
   name     = var.resource_group
   location = var.location
+
+    tags     = {
+    DeploymentId = "244467"
+    LaunchId     = "1345"
+    LaunchType   = "ON_DEMAND_LAB"
+    TemplateId   = "1243"
+    TenantId     = "203"
+  }
 }
